d87f39207f5ccd95e55fb8123ffa814b59a3177d
heldosouza <heldoaugusto@gmail.com>
2020-06-15 16:33:36 -0300

    creating HashProvider for tests

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,12 +1,18 @@
 import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -19,9 +25,14 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with repeated email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
 
-    const user = await createUser.execute({
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@gmail.com',
       password: '123456',
