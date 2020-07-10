5337184510d1a5f7427d9b84d44896c5f6e38461
heldosouza <heldoaugusto@gmail.com>
2020-07-10 17:22:55 -0300

    Validating routes with Celebrate

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
 import ResetPasswordController from '../controllers/ResetPasswordController';
@@ -7,7 +8,26 @@ const sessionsRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-sessionsRouter.post('/forgot', forgotPasswordController.create);
-sessionsRouter.post('/reset', resetPasswordController.create);
+sessionsRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create
+);
+
+sessionsRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create
+);
 
 export default sessionsRouter;
